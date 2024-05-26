e011bd9edc8b142c72c7d329df880ea8fe424e83
eredden <redden.evan@gmail.com>
2024-05-25 22:18:12 -0400

    rearranged code

--- src/main.py
+++ src/main.py
@@ -23,30 +23,29 @@ if __name__ == "__main__":
     group_ids = []
     minimum_common_users = 2
     results_file = "groups.txt"
-    time_to_wait = 0
+    time_to_wait = 0.5
 
     # DO NOT MODIFY ANYTHING BELOW THIS LINE!
 
+    # Get set of users from supplied groups.
     print("Retrieving users from each group...")
 
     user_ids = set()
 
-    # Get set of users from supplied groups.
     for group_id in group_ids:
-        group_user_ids = groups.get_members_of_group(group_id, user_limit)
-
-        for user_id in group_user_ids:
-            user_ids.add(user_id)
+        for id in groups.get_members_of_group(group_id, user_limit):
+            user_ids.add(id)
 
     print(f"{len(user_ids)} unique users were found.")
+
+    # Using multithreading to check multiple user groups at the same time.
     print("Retrieving user groups now...")
 
     results = {}
 
-    # Using multithreading to check multiple user groups at the same time.
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for user_id in user_ids:
-            executor.submit(groups.log_common_groups, user_id, results)
+        for id in user_ids:
+            executor.submit(groups.log_common_groups, id, results)
             time.sleep(time_to_wait)
 
     print(f"{len(results)} unique groups were found.")
@@ -60,15 +59,12 @@ if __name__ == "__main__":
         if results[group_id] < minimum_common_users:
             results.pop(group_id)
     
-    print(f"{len(results)} groups have more" 
-          f" than {minimum_common_users} members.")
+    print(f"{len(results)} groups have more than {minimum_common_users} members.")
     print(f"Logging results to {results_file}...")
 
     # Log results to desired text file.
     with open(results_file, "w") as log:
-        for group_id in results:
-            users = results[group_id]
-
-            log.write(f"https://www.roblox.com/groups/{group_id} - {users}\n")
+        for group_id in results: 
+            log.write(f"https://www.roblox.com/groups/{group_id} - {results[group_id]}\n")
     
     print(f"Results have been logged to {results_file}.")
